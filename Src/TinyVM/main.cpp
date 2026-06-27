32e90dda84f76544878f919c45f44406cdb8be73
not-flick <virajdeep46@gmail.com>
2026-06-28 04:03:37 +0530

    shell init

--- Src/TinyVM/main.cpp
+++ Src/TinyVM/main.cpp
@@ -1,22 +1,24 @@
-#ifndef PROJECT_ROOT
-#define PROJECT_ROOT "."
-#endif
-
 #include <iostream>
 #include <string>
 #include <filesystem>
+#include <fstream>
+#include <windows.h>
+
 
 namespace fs = std::filesystem;
 
 void create_directory(const std::string& username)
 {
     // PROJECT_ROOT is defined by CMake
-    fs::path disk = fs::path(PROJECT_ROOT) / "Disk";
+    fs::path disk = "C:/tiny_vm";
 
     fs::create_directories(disk / "bin");
     fs::create_directories(disk / "boot");
     fs::create_directories(disk / "dev");
     fs::create_directories(disk / "etc");
+
+    fs::create_directories(disk/ "shell");
+    fs::create_directories(disk / "system");
 
     fs::create_directories(disk / "home");
     fs::create_directories(disk / "home" / username);
@@ -50,7 +52,6 @@ void create_directory(const std::string& username)
 }
 
 
-
 int main()
 {
     std::string username;
@@ -59,6 +60,36 @@ int main()
     std::getline(std::cin, username);
 
     create_directory(username);
+
+    std::ofstream config("C:/tiny_vm/system/config.txt");
+
+    config << "username=" << username << '\n';
+    config << "hostname=tinyvm" << '\n';
+    config << "version=0.1" << '\n';
+    config << "cwd=c:/users/" << username << '\n';
+
+    config.close();
+
+    STARTUPINFOA si{};
+    PROCESS_INFORMATION pi{};
+
+    si.cb = sizeof(si);
+
+    CreateProcessA(
+        "C:\\Users\\viraj\\tiny_vm\\Build\\TinyShell.exe",
+        nullptr,
+        nullptr,
+        nullptr,
+        FALSE,
+        CREATE_NEW_CONSOLE,
+        nullptr,
+        "C:\\tiny_vm",
+        &si,
+        &pi
+    );
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
 
     return 0;
 }
